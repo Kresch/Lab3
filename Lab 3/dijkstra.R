d09dd042fb64152670a2ce6b4038828003310f2e
Kresch <niclas.lovsjo@me.com>
2015-09-07 11:53:32 +0200

    preps

--- Lab 3/dijkstra.R
+++ Lab 3/dijkstra.R
@@ -5,7 +5,7 @@ Wiki_graph <-
 
 ###
 
-dijkstra<-function(graph){
+dijkstra<-function(graph,init_node){
         find_neighbours<-function(node){
                 return(graph[,2][graph[,1]==node])
         }
@@ -29,14 +29,21 @@ dijkstra<-function(graph){
         generate_matrix<-function(graph){
                 nodes<-unique(graph[,1])
                 char_nodes<-as.character(nodes)
-                grph_mtrx<-matrix(c(rep(0,times=length(nodes)^2)),ncol=length(nodes))
+                grph_mtrx<-matrix(c(rep(Inf,times=length(nodes)^2)),ncol=length(nodes))
                 colnames(grph_mtrx)<-c(char_nodes)
+                rownames(grph_mtrx)<-c(char_nodes)
                 return(grph_mtrx)
         }
-        update_matrix <- function(matrix, current_node){
-                for(i in find_neighbours(current_node)){
-                        matrix[current_node,i] <- distance(current_node,i)
-                }
-                return(matrix)
-        }
+        
+        output<-generate_matrix(graph)
+        rownames(output)[init_node]<-as.character(init_node)
+        output[1,init_node]<-0
+        current_node<-init_node
+        
+        
+        find_neighbours(init_node)
+        minimum<-find_min(current_node)
+        distance(current_node,minimum)
+        
+        
 }
